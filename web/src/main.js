96b0aff9b965a4ec9ced7c98099145c7da6c5f51
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-03 21:38:50 +0800

    集成Ant Design VUE + 图标组件

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
 import store from './store'
+import 'ant-design-vue/dist/antd.css'; // 导入样式
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(app);
+app.use(Antd).use(store).use(router).mount('#app')
+
+// 全局使用图标
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
