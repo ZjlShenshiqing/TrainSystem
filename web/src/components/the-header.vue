d4df1602301504cfa9d885a4aa5fb0ef755d1fce
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-06 17:30:57 +0800

    完成公共组件的开发，实现顶部，左侧和main.vue的分离

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">张济霖</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+// 使用 ref 定义响应式数据
+const selectedKeys1 = ref(['1']); // 默认选中第一项
+</script>
+
+<style scoped>
+
+</style>
