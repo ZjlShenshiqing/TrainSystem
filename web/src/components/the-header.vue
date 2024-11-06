ef037157b1200f01475d05a88b898427a0d59820
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-06 22:48:13 +0800

    更改前端图标，使用VueX保存前端的token，最后使用VueX配合h5 session 缓存，解决浏览器刷新登录信息消失问题

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,12 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+    <div style="float: right; color: white;">
+      您好：{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white">
+        退出登录
+      </router-link>
+    </div>
     <a-menu
         v-model:selectedKeys="selectedKeys1"
         theme="dark"
@@ -16,8 +22,11 @@
 
 <script setup>
 import { ref } from "vue";
-// 使用 ref 定义响应式数据
-const selectedKeys1 = ref(['1']); // 默认选中第一项
+import store from "@/store";
+  // 使用 ref 定义响应式数据
+  const selectedKeys1 = ref(['1']); // 默认选中第一项
+  // 不用设置成响应式变量，因为不会修改member
+  let member = store.state.member;
 </script>
 
 <style scoped>
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,11 +1,20 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "MEMBER";
+
 export default createStore({
   state: {
+    // 去SessionStorage里面去读，如果里面有值就读，没有就是空对象
+    member: window.SessionStorage.get(MEMBER) || {}
   },
   getters: {
   },
   mutations: {
+    setMember (state, _member) {
+      state.member = _member;
+      // set的同时，放进SessionStorage中，以便拿出来
+      window.SessionStorage.set(MEMBER, _member)
+    }
   },
   actions: {
   },
