0712ba931d05f92ac34a15d0d441110439b17c0c
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-07 22:08:41 +0800

    通过Vue Router路由守卫增加路由登录拦截，访问控台主页现在需要登录，不登陆就跳回

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -14,7 +14,10 @@ const routes = [
   {
     path: '/',
     name: 'main',
-    component: () => import('../views/train-main.vue')
+    component: () => import('../views/train-main.vue'),
+    meta: {
+      loginRequire: true
+    },
   }
 ]
 
@@ -22,5 +25,26 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 增加路由跳转拦截,url发生变化时，进行拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  })) {
+    const _member = store.state.member;
+    console.log("页面登录校验开始：", _member);
+    if (!_member.token) {
+      console.log("用户未登录或登录超时！");
+      notification.error({ description: "未登录或登录超时" });
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
