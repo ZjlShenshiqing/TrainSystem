5ff72bcd2f9a08272e1ad1deeb47d722a87efbf3
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-07 14:44:34 +0800

    为gateway模块的JwtUtil增加异常处理

--- train-gateway/src/main/java/com/zjl/train/gateway/util/JwtUtil.java
+++ train-gateway/src/main/java/com/zjl/train/gateway/util/JwtUtil.java
@@ -43,13 +43,18 @@ public class JwtUtil {
     }
 
     public static boolean validate(String token) {
-        LOG.info("开始JWT token校验，token：{}", token);
-        GlobalBouncyCastleProvider.setUseBouncyCastle(false);
-        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
-        // validate包含了verify
-        boolean validate = jwt.validate(0);
-        LOG.info("JWT token校验结果：{}", validate);
-        return validate;
+        try {
+            LOG.info("开始JWT token校验，token：{}", token);
+            GlobalBouncyCastleProvider.setUseBouncyCastle(false);
+            JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+            // validate包含了verify
+            boolean validate = jwt.validate(0);
+            LOG.info("JWT token校验结果：{}", validate);
+            return validate;
+        } catch (Exception e) {
+            LOG.error("JWT token校验结果异常：{}", e);
+            return false;
+        }
     }
 
     public static JSONObject getJSONObject(String token) {
