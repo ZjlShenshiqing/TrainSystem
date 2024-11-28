5ad9b150f9d2b3690a52ac8fc2a84d28b649f221
Zhangjilin <168534039+ZjlShenshiqing@users.noreply.github.com>
2024-11-28 17:47:38 +0800

    增加batch定时调度模块

---
+++ train-batchtask/src/main/java/com/zjl/train/batchtask/config/BatchTaskApplication.java
@@ -0,0 +1,23 @@
+package com.zjl.train.batchtask.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.zjl")
+@MapperScan("com.zjl.train.*.mapper")
+public class BatchTaskApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BatchTaskApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchTaskApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("测试地址：\thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+    }
+}
